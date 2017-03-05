4f0bc62fd51a67db27253343233b7501b83e082b
FrancoisYapo <francoislogiciel@gmail.com>
2017-03-05 10:31:35 -1100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php require 'partials/header.php'; ?>
+	<?php require 'partials/navbar.php'; ?>
+
+	<?php require 'partials/banner.php'; ?>
+
+				<!-- //Modal1 -->
+			<!--/content-inner-section-->
+				<div class="w3_content_agilleinfo_inner">
+					<div class="agile_featured_movies">
+						<!--/agileinfo_tabs-->
+					   <div class="agileinfo_tabs">
+						<!--/tab-section-->
+						<?php require 'partials/tab-section.php'; ?>
+				<!--//tab-section-->	
+				  <h3 class="agile_w3_title"> Derniers  <span>films</span></h3>
+			<!--/movies-->				
+			<?php require 'partials/movies-laste-movies.php'; ?>
+
+
+					
+				<!--//top-movies-->
+			</div>
+		</div>
+			<!--//content-inner-section-->
+		
+	<!--/footer-bottom-->
+<?php require 'partials/footer.php'; ?>
